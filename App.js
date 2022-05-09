277d55bd4c213259a0a2d1a99d376ac3a2d1dd43
AdonayMejia <89654943+AdonayMejia@users.noreply.github.com>
2022-05-08 19:45:18 -0600

    add depenencie

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
-
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './app/Navigations/Navigation';
-import { firebaseApp } from './app/utils/firebase';
-import * as firebase from 'firebase';
-import Main from "./main"
 
 export default function App() {
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log(user);
-    });
-  }, []);
-  return <Main />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
